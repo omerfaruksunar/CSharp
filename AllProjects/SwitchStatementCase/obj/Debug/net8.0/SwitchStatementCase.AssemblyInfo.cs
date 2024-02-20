e3c98f8e84c7d1dd33c8a744a1ebc942e2f21dd3
omerfaruksunar <omerfaruksunar@gmail.com>
2024-02-20 18:30:09 +0300

    My-C#-Works Day 5

--- AllProjects/SwitchStatementCase/obj/Debug/net8.0/SwitchStatementCase.AssemblyInfo.cs
+++ AllProjects/SwitchStatementCase/obj/Debug/net8.0/SwitchStatementCase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwitchStatementCase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8774bbfe539914cd50b689035336327313440ebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d232a9dcb3dacbdb7b80c48a69c000a55dbd2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwitchStatementCase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwitchStatementCase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
